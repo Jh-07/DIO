eb1c903ed2b98d7c39ae93aa6a9b248bec99028f
Jh07 <jonathasmas2@gmail.com>
2022-05-05 23:31:35 -0300

    Desafio #1: Adicionar Eventlistener e uma condição

--- Exercicios/Java/assets/scripts.js
+++ Exercicios/Java/assets/scripts.js
@@ -1,10 +1,25 @@
 var x = document.getElementById('currentNumber')
 var cx = 0;
 function increment(){
+    if(cx<10){
     cx = cx +1;
     x.innerHTML= cx;
+    }
+    else{
+        alert("Não pode ser maior que 10");
+    }
 }
 function decrement(){
+    if(cx>0){
     cx = cx -1;
     x.innerHTML= cx;
+    }
+    else{
+        alert("Não pode ser menor que 0");
+    }
 }
+document.getElementById("adicionar").addEventListener("click",increment);
+document.getElementById("subtrair").addEventListener("click",decrement);
+
+
+
