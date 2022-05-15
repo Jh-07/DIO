263caabc6b361800744b2ff90b5834fb3d89a7bf
Jh07 <jonathasmas2@gmail.com>
2022-05-14 22:13:53 -0300

    Exercicio #24: Utilizando modulos e importando scripts Java

---
+++ Exercicios/Modulos/main.mjs
@@ -0,0 +1,4 @@
+import * as funcoes from "./funcoes.mjs";
+console.log(funcoes.mostraCidade('Jose','Rj'))
+console.log(funcoes.mostraIdade('Augusto',30))
+console.log(funcoes.mostraHobby('Fernando','Videogame'))
